0f89a307ff666a45850640e4f055b1bfb98c6938
Victor Fernandez de Alba <victor.fernandez@upcnet.es>
2012-09-06 17:40:54 +0200

    first work on the base archetype

--- src/archetypes/testcase/content/simpletype.py
+++ src/archetypes/testcase/content/simpletype.py
@@ -14,7 +14,136 @@ from archetypes.testcase.config import PROJECTNAME
 
 SimpleTypeSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
 
-    # -*- Your Archetypes field definitions here ... -*-
+    atapi.TextField(
+        name='body',
+        required=True,
+        searchable=True,
+        default_output_type='text/html',
+        allowable_content_types=(
+            'text/plain',
+            'text/restructured',
+            'text/html',
+            'application/msword',
+        ),
+        widget=atapi.RichWidget(
+            description='Enter or upload text for the body of the document.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='contactName',
+        languageIndependent=True,
+        widget=atapi.StringWidget(
+            description='Enter a contact person.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='contactName2',
+        languageIndependent=True,
+        widget=atapi.StringWidget(
+            description='Enter a contact person.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='contactName3',
+        languageIndependent=True,
+        widget=atapi.StringWidget(
+            description='Enter a contact person.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='contactName4',
+        languageIndependent=False,
+        accessor='getFourthContactName',
+        mutator='setFourthContactName',
+        widget=atapi.StringWidget(
+            description='This field has custom accessor and mutator.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='contactName5',
+        languageIndependent=True,
+        accessor='getFifthContactName',
+        mutator='setFifthContactName',
+        widget=atapi.StringWidget(
+            description='This field has custom accessor and mutator.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='langIndependentInBase',
+        languageIndependent=True,
+        widget=atapi.StringWidget(
+            description='This field is language independent in SimpleType.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='langIndependentInDerived',
+        languageIndependent=False,
+        widget=atapi.StringWidget(
+            description='This field is language dependent in DerivedType.',
+        ),
+    ),
+
+    atapi.StringField(
+        name='langIndependentInBoth',
+        languageIndependent=True,
+        widget=atapi.StringWidget(
+            description='This field is language independent everywhere.',
+        ),
+    ),
+
+    atapi.ImageField(
+        name='image',
+        languageIndependent=True,
+    ),
+
+    atapi.ImageField(
+        name='imageDependent',
+        languageIndependent=False,
+    ),
+
+    atapi.ReferenceField(
+        name='reference',
+        allowed_types=('SimpleType', ),
+        languageIndependent=True,
+        relationship='referenceType',
+    ),
+
+    atapi.ReferenceField(
+        name='referenceDependent',
+        allowed_types=('SimpleType', ),
+        languageIndependent=False,
+        relationship='referenceDependentType',
+    ),
+
+    atapi.ReferenceField(
+        name='referenceMulti',
+        allowed_types=('SimpleType', ),
+        languageIndependent=True,
+        multiValued=True,
+        relationship='referenceType',
+    ),
+
+    atapi.LinesField(
+        name='lines',
+        languageIndependent=True,
+        widget=atapi.LinesWidget(label="Lines"),
+    ),
+
+    atapi.TextField(
+        name='neutralText',
+        languageIndependent=True,
+        default_output_type='text/html',
+        widget=atapi.RichWidget(
+            description='Enter some text',
+        ),
+    ),
 
 ))
 
@@ -29,5 +158,33 @@ class SimpleType(base.ATCTContent):
     meta_type = "SimpleType"
     schema = SimpleTypeSchema
 
+    # def setContactName(self, value, **kw):
+    #     """Set contact name.
+    #     This tests language independent method generation
+    #     """
+    #     self.getField('contactName').set(self, value, **kw)
+    #     self.testing = value
+
+    def getFourthContactName(self):
+        """Custom accessor."""
+        return 'getFourthContactName'
+
+    def setFourthContactName(self, value, **kw):
+        """Custom mutator."""
+        self.getField('contactName4').set(self, 'cn4 ' + value, **kw)
+
+    def getFifthContactName(self):
+        """Custom accessor."""
+        return 'getFifthContactName'
+
+    def setFifthContactName(self, value, **kw):
+        """Custom mutator."""
+        self.getField('contactName5').set(self, 'cn5 ' + value, **kw)
+
+    def getRawReference(self):
+        return self.getField('reference').getRaw(self)
+
+    def getRawReferenceDependent(self):
+        return self.getField('referenceDependent').getRaw(self)
 
 atapi.registerType(SimpleType, PROJECTNAME)
